87c41f560a8952800f3670ea3571d8a78c91207b
Thang0226 <thang.nd0226@gmail.com>
2025-01-26 13:14:55 +0700

    add image using icon code & add styling

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export async function getStaticProps() {
     const res = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=21.028511&lon=105.804817&appid=30de923456283628da276346257b5d19')
     const repo = await res.json()
@@ -6,10 +8,8 @@ export async function getStaticProps() {
 
 export default function Home({repo}) {
 
-    // export default function Page({ repo }) {
-    //     return repo.stargazers_count
-    // }
     console.log(repo);
+    let iconCode = repo.weather[0].icon;
 
     return (
         <>
@@ -18,12 +18,14 @@ export default function Home({repo}) {
                 <tbody>
                 <tr>
                     <td className="city">{repo.name}</td>
-                    <td className="icon">{repo.weather[0].icon}</td>
-                    <td className="temperature">{Math.round(repo.main.temp - 273.15)} ℃</td>
+                    <td className="icon">
+                        <Image src={`https://openweathermap.org/img/wn/${iconCode}@2x.png`} alt={"Weather image"} width={110} height={110} />
+                    </td>
+                    <td className="temperature">{Math.round(repo.main.temp - 273.15)}<span className="degree"> ℃</span></td>
                     <td className="details">
-                        Weather description: <span style={{fontWeight: "bold"}}>{repo.weather[0].description}</span><br/>
-                        Humidity: {repo.main.humidity} %<br/>
-                        Wind speed: {repo.wind.speed} m/s
+                        Weather: <span style={{fontWeight: "bold"}}>{repo.weather[0].description}</span><br/>
+                        Humidity: <span style={{fontWeight: "bold"}}>{repo.main.humidity} %</span><br/>
+                        Wind speed: <span style={{fontWeight: "bold"}}>{repo.wind.speed} m/s</span>
                     </td>
                 </tr>
                 </tbody>
